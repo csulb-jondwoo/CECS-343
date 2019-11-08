e4189e2b995c734d17bce276dc980954a942b6f7
hueletl <47196575+hueletl@users.noreply.github.com>
2019-11-07 16:10:58 -0800

    adds NetBeans project KOT

---
+++ Implementation/KOT/src/DisplayImage/Monster.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DisplayImage;
+
+/**
+ *
+ * @author huele
+ */
+public class Monster {
+    private String name;
+    private int HP;
+    private int VP;
+    private int EP;
+    
+    
+    
+}
